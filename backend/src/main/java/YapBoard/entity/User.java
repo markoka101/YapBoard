cc4c81907e67fe8cfa200ebceed624d5691b22fd
markoka101 <markoka101@gmail.com>
2024-04-22 18:27:13 -0400

    removed unused import

--- backend/src/main/java/YapBoard/entity/User.java
+++ backend/src/main/java/YapBoard/entity/User.java
@@ -1,6 +1,5 @@
 package YapBoard.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
