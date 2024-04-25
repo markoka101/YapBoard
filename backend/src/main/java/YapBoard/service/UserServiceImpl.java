ee1bb70b466ce6b65f604368901c316bc6ef2591
markoka101 <markoka101@gmail.com>
2024-04-25 18:05:17 -0400

    logic to delete user

--- backend/src/main/java/YapBoard/service/UserServiceImpl.java
+++ backend/src/main/java/YapBoard/service/UserServiceImpl.java
@@ -5,8 +5,13 @@ import YapBoard.entity.User;
 import YapBoard.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -67,7 +72,9 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<User> getAllUsers(int page) {
-        return null;
+        Pageable pageable = PageRequest.of(page,5);
+        Page<User> users = userRepository.findAll(pageable);
+        return users.getContent();
     }
 
     @Override
@@ -87,7 +94,9 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void deleteUser(Long id) {
-
+        Optional<User> user = userRepository.findById(id);
+        User unwrappedUser = unwrapUser(user);
+        userRepository.delete(unwrappedUser);
     }
 
     static User unwrapUser(Optional<User> entity) {
