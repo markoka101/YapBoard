e96f24c55d2ad1bd783ef4d539e0a8431395efc7
markoka101 <markoka101@gmail.com>
2024-06-21 16:18:27 -0400

    implemented some post methods

--- backend/src/main/java/YapBoard/service/PostsServiceImpl.java
+++ backend/src/main/java/YapBoard/service/PostsServiceImpl.java
@@ -2,53 +2,75 @@ package YapBoard.service;
 
 import YapBoard.entity.Posts;
 import YapBoard.repository.PostsRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class PostsServiceImpl implements PostsService{
     private PostsRepository postsRepository;
     private UserService userService;
+
+    //create post
     @Override
     public void createPost(Posts posts) {
-
+        postsRepository.save(posts);
     }
 
+    //edit post
     @Override
     public void editPost(Posts posts) {
 
     }
 
+    //delete post
     @Override
     public void deletePost(Long id) {
-
+        Optional<Posts> post = postsRepository.findById(id);
+        Posts unwrappedPost = unwrapPost(post);
+        postsRepository.delete(unwrappedPost);
     }
 
+    //get posts by user
     @Override
     public List<Posts> getUserPosts(Long userId, int page, String direction) {
         return null;
     }
 
+    //get every post
     @Override
     public List<Posts> getAllPosts(int page) {
         return null;
     }
 
+    //get posts by title
     @Override
     public List<Posts> getPostsByTitle(String title, int page, String direction) {
         return null;
     }
 
+    //get posts from followed users
     @Override
     public List<Posts> getFollowedPosts(Long userId, int page, String direction) {
         return null;
     }
 
+    //get posts by their tags
     @Override
     public List<Posts> getByTag(String tag, int page, String direction) {
         return null;
+    }
+
+    //unwrap optional to Posts object
+    static Posts unwrapPost(Optional<Posts> entity) {
+        if (entity.isPresent()) {
+            return entity.get();
+        } else {
+            throw new EntityNotFoundException("Post not found");
+        }
     }
 }
