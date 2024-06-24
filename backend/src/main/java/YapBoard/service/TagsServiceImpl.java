1077a8f37826449c2899400a75915ee62790d127
markoka101 <markoka101@gmail.com>
2024-06-24 16:53:59 -0400

    completed tags service implementation

--- backend/src/main/java/YapBoard/service/TagsServiceImpl.java
+++ backend/src/main/java/YapBoard/service/TagsServiceImpl.java
@@ -1,6 +1,7 @@
 package YapBoard.service;
 
 import YapBoard.entity.Tags;
+import YapBoard.repository.TagsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +10,16 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class TagsServiceImpl implements TagsService{
+    TagsRepository tagsRepository;
+    //create tag
     @Override
     public void createTag(Tags tags) {
-
+        tagsRepository.save(tags);
     }
 
+    //find all tags
     @Override
     public List<Tags> getAllTags() {
-        return null;
+        return tagsRepository.findAll();
     }
 }
