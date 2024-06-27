403f7f1bb99a175231f3981e51ef350ee76a78a6
markoka101 <markoka101@gmail.com>
2024-06-27 15:51:54 -0400

    implemented follow and unfollow methods

--- backend/src/main/java/YapBoard/service/FollowServiceImpl.java
+++ backend/src/main/java/YapBoard/service/FollowServiceImpl.java
@@ -1,19 +1,21 @@
 package YapBoard.service;
 
 import YapBoard.entity.Follow;
+import YapBoard.repository.FollowRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class FollowServiceImpl implements FollowService {
+    private FollowRepository followRepository;
     @Override
     public void followUser(Follow follow) {
-
+        followRepository.save(follow);
     }
 
     @Override
     public void unfollow(Follow follow) {
-
+        followRepository.delete(follow);
     }
 }
