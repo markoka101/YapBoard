2b72621bc210cfc6ddac416eec5aa9275eba6064
markoka101 <markoka101@gmail.com>
2024-07-02 14:45:01 -0400

    implemented save and delete

--- backend/src/main/java/YapBoard/service/LikeServiceImpl.java
+++ backend/src/main/java/YapBoard/service/LikeServiceImpl.java
@@ -1,6 +1,7 @@
 package YapBoard.service;
 
 import YapBoard.entity.Like;
+import YapBoard.repository.LikeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,15 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class LikeServiceImpl implements LikeService{
+    LikeRepository likeRepository;
     @Override
     public void likePost(Like like) {
-
+        likeRepository.save(like);
     }
 
     @Override
     public void deleteLiked(Like like) {
-
+        likeRepository.delete(like);
     }
 
     @Override
