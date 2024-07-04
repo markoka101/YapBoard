d60691ff411a3d2d1ae73e59d1d31b80db57dc9b
markoka101 <markoka101@gmail.com>
2024-07-04 16:51:27 -0400

    implement pagination for finding likes

--- backend/src/main/java/YapBoard/repository/LikeRepository.java
+++ backend/src/main/java/YapBoard/repository/LikeRepository.java
@@ -1,7 +1,13 @@
 package YapBoard.repository;
 
 import YapBoard.entity.Like;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like,Long> {
+    Page<Like> findAllByPost(Long postId, Pageable pageable);
+    Page<Like> findAllByUser(Long userId, Pageable pageable);
+    Optional<Like> findByUserAndPost(Long userId,Long postId);
 }
