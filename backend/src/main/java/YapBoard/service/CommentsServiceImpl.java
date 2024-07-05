29150c00969de29bdf38fccecce47b978496d61c
markoka101 <markoka101@gmail.com>
2024-07-05 16:17:15 -0400

    implemented methods for comments

--- backend/src/main/java/YapBoard/service/CommentsServiceImpl.java
+++ backend/src/main/java/YapBoard/service/CommentsServiceImpl.java
@@ -1,36 +1,62 @@
 package YapBoard.service;
 
 import YapBoard.entity.Comments;
+import YapBoard.repository.CommentsRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class CommentsServiceImpl implements CommentsService{
+    CommentsRepository commentsRepository;
+    //save comment to repo
     @Override
     public void createComment(Comments comments) {
-
+        commentsRepository.save(comments);
     }
 
+    //edit comment
     @Override
     public void editComment(Comments comments) {
-
+        commentsRepository.save(comments);
     }
 
+    //delete comment
     @Override
     public void deleteComment(Long id) {
-
+        Comments comment = unwrap(commentsRepository.findById(id));
+        commentsRepository.delete(comment);
     }
 
+    //find comments from user
     @Override
     public List<Comments> userComments(Long userId, int page) {
-        return null;
+        Pageable pageable = PageRequest.of(page,10);
+        Page<Comments> comments = commentsRepository.findByUser(userId,pageable);
+        return comments.getContent();
     }
 
+    //find comments by post
     @Override
     public List<Comments> postComments(Long postId, int page) {
-        return null;
+        Pageable pageable = PageRequest.of(page,10);
+        Page<Comments> comments = commentsRepository.findByPosts(postId,pageable);
+        return comments.getContent();
+    }
+
+    //unwrap optional comment
+    static Comments unwrap(Optional<Comments> entity) {
+        if (entity.isPresent()) {
+            return entity.get();
+        }
+
+        throw new EntityNotFoundException("Comment not found");
     }
 }
