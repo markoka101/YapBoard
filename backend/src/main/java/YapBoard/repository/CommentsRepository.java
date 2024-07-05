28faf5b33668e8fde9c735325c010f00b6ceaba3
markoka101 <markoka101@gmail.com>
2024-07-05 16:16:32 -0400

    allow to find by posts or user

--- backend/src/main/java/YapBoard/repository/CommentsRepository.java
+++ backend/src/main/java/YapBoard/repository/CommentsRepository.java
@@ -1,7 +1,11 @@
 package YapBoard.repository;
 
 import YapBoard.entity.Comments;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentsRepository extends JpaRepository<Comments,Long> {
+    Page<Comments>  findByUser(Long userId, Pageable pageable);
+    Page<Comments> findByPosts(Long postId, Pageable pageable);
 }
