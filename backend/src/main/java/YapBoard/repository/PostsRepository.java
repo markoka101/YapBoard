683c34a6517cec19d7cbdfe003acedb8d115c4f1
markoka101 <markoka101@gmail.com>
2024-07-08 16:21:01 -0400

    find posts by user and title

--- backend/src/main/java/YapBoard/repository/PostsRepository.java
+++ backend/src/main/java/YapBoard/repository/PostsRepository.java
@@ -1,8 +1,15 @@
 package YapBoard.repository;
 
 import YapBoard.entity.Posts;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+    Page<Posts> findByTitleLike(String title, Pageable pageable);
+    Page<Posts> findByUser(Long user_id, Pageable pageable);
+
 }
