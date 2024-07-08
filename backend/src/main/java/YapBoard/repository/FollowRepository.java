e973d3dc68bdb1a338e7bc669296e281212148e2
markoka101 <markoka101@gmail.com>
2024-07-08 16:19:56 -0400

    find by user and following

--- backend/src/main/java/YapBoard/repository/FollowRepository.java
+++ backend/src/main/java/YapBoard/repository/FollowRepository.java
@@ -1,7 +1,12 @@
 package YapBoard.repository;
 
 import YapBoard.entity.Follow;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow,Long> {
+    Page<Follow> findByUser(Long user_id);
+    Optional<Follow> findByUserAndFollowing(Long user_id,Long id);
 }
